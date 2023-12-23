7f0a0779b0af037721a70f215f6d332a44b8a1da
mamun8893 <0316361936@student.wub.edu.bd>
2023-12-23 23:12:06 +0600

    completed problem 1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -2,24 +2,86 @@ import React, { useState } from "react";
 
 const Problem1 = () => {
   const [show, setShow] = useState("all");
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Task 1", status: "Active" },
+    { id: 2, name: "Task 2", status: "Completed" },
+    { id: 3, name: "Task 3", status: "Pending" },
+    // Add more tasks as needed
+  ]);
 
   const handleClick = (val) => {
     setShow(val);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.elements.name.value;
+    const status = e.target.elements.status.value;
+
+    // Add a new task to the list
+    setTasks([...tasks, { id: tasks.length + 1, name, status }]);
+    // Reset the form
+    e.target.reset();
+  };
+
+  const getFilteredTasks = () => {
+    switch (show) {
+      case "active":
+        return tasks.filter((task) => task.status.toLowerCase() === "active");
+      case "completed":
+        return tasks.filter(
+          (task) => task.status.toLowerCase() === "completed"
+        );
+      case "pending":
+        return tasks.filter((task) => task.status.toLowerCase() === "pending");
+      case "archive":
+        return tasks.filter((task) => task.status.toLowerCase() === "archive");
+      case "all":
+      default:
+        return tasks.sort((a, b) => {
+          const order = ["active", "completed", "pending", "archive"];
+
+          const getStatusOrder = (status) => {
+            const index = order.indexOf(status);
+            return index !== -1 ? index : order.length;
+          };
+
+          const compare =
+            getStatusOrder(a.status.toLowerCase()) -
+            getStatusOrder(b.status.toLowerCase());
+
+          if (compare === 0) {
+            // If statuses are the same, sort by ID (newest first)
+            return b.id - a.id;
+          }
+
+          return compare;
+        });
+    }
   };
 
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
-        <div className="col-6 ">
-          <form className="row gy-2 gx-3 align-items-center mb-4">
+        <div className="col-6">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
             <div className="col-auto">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                placeholder="Name"
+              />
             </div>
             <div className="col-auto">
               <input
                 type="text"
                 className="form-control"
+                name="status"
                 placeholder="Status"
               />
             </div>
@@ -59,17 +121,43 @@ const Problem1 = () => {
                 Completed
               </button>
             </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "pending" && "active"}`}
+                type="button"
+                onClick={() => handleClick("pending")}
+              >
+                Pending
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "archive" && "active"}`}
+                type="button"
+                onClick={() => handleClick("archive")}
+              >
+                Archive
+              </button>
+            </li>
           </ul>
-          <div className="tab-content"></div>
-          <table className="table table-striped ">
-            <thead>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Status</th>
-              </tr>
-            </thead>
-            <tbody></tbody>
-          </table>
+          <div className="tab-content">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {getFilteredTasks().map((task) => (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
